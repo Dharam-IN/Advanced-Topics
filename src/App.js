2a1af29b26598d603a64655b5183b212464b9062
dharamraj-dev <dharamrajyadav6312@gmail.com>
2024-02-09 04:42:47 +0530

    Setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Result from './Components/Result';
+import Search from './Components/Search';
+
+function App() {
+  return (
+    <>
+      <Search/>
+      <Result/>
+    </>
+  );
+}
+
+export default App;
